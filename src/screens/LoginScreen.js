ec8cf3cbf3ca5e6df87ec63d0527f8f14d5976f8
Jimmy Lin <jimmy.lin0402@gmail.com>
2021-05-19 13:38:26 -0700

    Working on Sqlite schema.

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+const LoginScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text
+      style={styles.text}>Jimmy
+      </Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default LoginScreen;
+
